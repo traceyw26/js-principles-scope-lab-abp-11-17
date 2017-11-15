92a799c5afc2bdc13c2b1ca1b2460c2479511733
Tracey <tracey.wilhelmsen@gmail.com>
2017-11-15 01:46:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var customerName = "bob";
 function upperCaseCustomerName() {
   return customerName = customerName.toUpperCase()
 }
+
+function setBestCustomer() {
+  var bestCustomer = "not bob";
+}
